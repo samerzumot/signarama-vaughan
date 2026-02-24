7c3a5d5e4c1c8e108a989253b96d2a494106440e
Samer Zumot <54731842+samerzumot@users.noreply.github.com>
2026-02-24 09:36:18 -0800

    Enable reCAPTCHA and use unique file input names for attachments

--- app/components/QuoteForm.tsx
+++ app/components/QuoteForm.tsx
@@ -78,7 +78,6 @@ export function QuoteForm({
         className="space-y-4"
       >
         <input type="hidden" name="_subject" value="New Quote Request - Signarama Vaughan" />
-        <input type="hidden" name="_captcha" value="false" />
         <input type="hidden" name="_template" value="table" />
         <input type="hidden" name="_next" value="https://www.custombusinesssigns.ca/thank-you" />
         <input type="text" name="_honey" style={{ display: "none" }} />
@@ -121,7 +120,7 @@ export function QuoteForm({
             <input
               ref={fileInputRef}
               type="file"
-              name="attachment"
+              name="attachment1"
               multiple
               accept="image/*,application/pdf"
               onChange={handleFileChange}
@@ -153,7 +152,7 @@ export function QuoteForm({
             <input
               ref={sitePhotoRef}
               type="file"
-              name="attachment"
+              name="attachment2"
               multiple
               accept="image/*"
               onChange={handleSitePhotoChange}
